18618089e7c44bb7505f3a72cd284ce33c75c16d
mlucero <mauricio.lucero2014@gmail.com>
2021-10-06 16:45:18 -0300

    New bill form page

--- src/pages/Bill/index.jsx
+++ src/pages/Bill/index.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 
 import { Button, TextField, Grid, Paper, makeStyles } from "@material-ui/core";
 import { KeyboardDatePicker } from "@material-ui/pickers";
+import { saveBill } from "services/billService";
+import { UserContext } from "context/UserContext";
+import { useForm } from "hooks/useForm";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -19,34 +22,99 @@ const useStyles = makeStyles((theme) => ({
 export const Bill = () => {
   const classes = useStyles();
 
-  const [selectedDate, handleDateChange] = useState(new Date());
+  const { user } = useContext(UserContext);
+
+  const { formData: billForm, handleInputChange, reset } = useForm({
+    name: "",
+    category: "",
+    expirationDate: new Date(),
+    amount: 0,
+  });
+
+  const { name, category, expirationDate, amount } = billForm;
+
+  const handleDateChange = (date) => {
+    handleInputChange({
+      target: {
+        name: "expirationDate",
+        value: date,
+      },
+    });
+  };
+
+  const handleSave = () => {
+    saveBill({
+      userId: user.uid,
+      name,
+      category,
+      expirationDate,
+      amount,
+    }).then()
+
+
+  };
+
   return (
+    <div className="flex-centered-container">
       <form autoComplete="off">
         <Paper className={classes.paper}>
           <h1>Bill</h1>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} alignItems="center">
-              <TextField className={classes.input} id="Name" label="Name" fullWidth />
-            </Grid>
-            <Grid item xs={12} sm={6} alignItems="center">
-              <KeyboardDatePicker
+            <Grid container item xs={12} sm={6} alignItems="center">
+              <TextField
+                className={classes.input}
                 fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                onChange={handleInputChange}
+                value={name}
+              />
+            </Grid>
+            <Grid container item xs={12} sm={6} alignItems="center">
+              <TextField
+                className={classes.input}
+                fullWidth
+                id="category"
+                label="Category"
+                name="category"
+                onChange={handleInputChange}
+                value={category}
+              />
+            </Grid>
+            <Grid container item xs={12} sm={6} alignItems="center">
+              <KeyboardDatePicker
                 disableToolbar
-                variant="inline"
                 format="do LLL"
-                margin="normal"
-                id="date-picker-inline"
+                fullWidth
+                id="expiration-date"
                 label="Expiration Date"
-                value={selectedDate}
+                margin="normal"
+                name="expirationDate"
+                value={expirationDate}
+                variant="inline"
                 onChange={handleDateChange}
                 KeyboardButtonProps={{
                   "aria-label": "change date",
                 }}
               />
             </Grid>
+            <Grid container item xs={12} sm={6} alignItems="center">
+              <TextField
+                className={classes.input}
+                fullWidth
+                id="amount"
+                label="Amount"
+                name="amount"
+                onChange={handleInputChange}
+                value={amount}
+                type="number"
+              />
+            </Grid>
             <Grid item xs={12}>
               <Button
                 className={classes.button}
+                onClick={handleSave}
                 variant="contained"
                 color="primary"
                 fullWidth
@@ -57,5 +125,6 @@ export const Bill = () => {
           </Grid>
         </Paper>
       </form>
+    </div>
   );
 };
