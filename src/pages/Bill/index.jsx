85fb688beddc6aef9a8e220ef067fb26cdf4649b
mlucero <mauricio.lucero2014@gmail.com>
2021-10-11 03:37:14 -0300

    Add select input for category

--- src/pages/Bill/index.jsx
+++ src/pages/Bill/index.jsx
@@ -1,18 +1,38 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 
-import { Button, TextField, Grid, Paper, makeStyles } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  makeStyles,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 import { KeyboardDatePicker } from "@material-ui/pickers";
 import { saveBill } from "services/billService";
 import { UserContext } from "context/UserContext";
 import { useForm } from "hooks/useForm";
+import { ItemIcon } from "components/Bills/Item/Icon";
+import { CATEGORIES_MAPPER } from "config";
 
 const useStyles = makeStyles((theme) => ({
   button: {
     marginTop: theme.spacing(2),
   },
+  formControl: {
+    width: "100%",
+  },
   input: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(1),
+  },
+  select: {
+    display: "flex",
+    alignItems: "center",
+    gap: "4px",
   },
   paper: {
     padding: theme.spacing(2),
@@ -24,7 +44,11 @@ export const Bill = () => {
 
   const { user } = useContext(UserContext);
 
-  const { formData: billForm, handleInputChange, reset } = useForm({
+  const {
+    formData: billForm,
+    handleInputChange,
+    reset,
+  } = useForm({
     name: "",
     category: "",
     expirationDate: new Date(),
@@ -49,9 +73,7 @@ export const Bill = () => {
       category,
       expirationDate,
       amount,
-    }).then()
-
-
+    }).then(() => reset());
   };
 
   return (
@@ -72,15 +94,28 @@ export const Bill = () => {
               />
             </Grid>
             <Grid container item xs={12} sm={6} alignItems="center">
-              <TextField
-                className={classes.input}
-                fullWidth
-                id="category"
-                label="Category"
-                name="category"
-                onChange={handleInputChange}
-                value={category}
-              />
+              <FormControl className={classes.formControl}>
+                <InputLabel id="category">Category</InputLabel>
+                <Select
+                  labelId="category"
+                  id="category-select"
+                  name="category"
+                  value={category}
+                  onChange={handleInputChange}
+                  renderValue={(value) => (
+                    <div className={classes.select}>
+                      <ItemIcon category={value} fontSize="small" />
+                      {CATEGORIES_MAPPER[value].displayName}{" "}
+                    </div>
+                  )}
+                >
+                  {Object.keys(CATEGORIES_MAPPER).map((key, index) => (
+                    <MenuItem value={key} key={index}>
+                      {CATEGORIES_MAPPER[key].displayName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
             </Grid>
             <Grid container item xs={12} sm={6} alignItems="center">
               <KeyboardDatePicker
