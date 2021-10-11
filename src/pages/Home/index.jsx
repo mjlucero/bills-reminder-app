6ab271331415a7d54682bd25f46ef891daa39311
mlucero <mauricio.lucero2014@gmail.com>
2021-10-11 03:36:21 -0300

    Include new bills list component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles, Fab } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
-import { Header } from "components/Header";
-import { Link } from "react-router-dom";
+
+import { getBills } from "services/billService";
+import { UserContext } from "context/UserContext";
+import { BillsList } from "components/Bills/List";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,20 +14,43 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     height: "100%",
   },
+  addButtonContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: theme.spacing(2),
+    width: "100%",
+  },
+  addButton: {
+    height: "56px",
+    width: "56px",
+  },
 }));
 
 export const Home = () => {
   const classes = useStyles();
 
+  const { user } = useContext(UserContext);
+
+  const [unpaidBills, setUnpaidBills] = useState([]);
+
+  useEffect(() => {
+    getBills(user.uid).then((bills) => {
+      setUnpaidBills(bills);
+    });
+  }, [user.uid]);
+
   return (
     <div className={classes.root}>
       <main>
         <h1>Home</h1>
-        <Link to="/bill">
-          <Fab color="primary" aria-label="add">
-            <Add />
-          </Fab>
-        </Link>
+        <BillsList bills={unpaidBills} />
+        <div className={classes.addButtonContainer}>
+          <Link to="/bill" className={classes.addButton}>
+            <Fab color="primary" aria-label="add">
+              <Add />
+            </Fab>
+          </Link>
+        </div>
       </main>
     </div>
   );
